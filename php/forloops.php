cf1ac0d120c7bb4c12124f4da8d939738f8e98db
shakthi <shakthirm05>
2023-06-23 18:36:18 +0530

    changes

---
+++ php/forloops.php
@@ -0,0 +1,6 @@
+<?php
+$x=1;
+for($x=0;$x<=10;$x++){
+    echo "the number is:$x <br>";
+}
+?>
---
+++ php/whileloop.php
@@ -0,0 +1,12 @@
+<?php
+$x=1;
+while($x<=5){
+    echo"the number is:$x<br>";
+    $x++;
+
+}
+while($x<=100){
+    echo" the number is :$x<br>";
+    $x+=10;
+}
+?>
